9c4b9f205f492e18e4fa9819f40b47171ba3bc16
Vimarsh Puneet <khushi@Vimarshs-MacBook-Pro-2.local>
2012-10-16 15:26:10 -0700

    Initial Commit

---
+++ LearningDatabase/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LearningDatabase
+//
+//  Created by Vimarsh Puneet on 10/16/12.
+//  Copyright (c) 2012 Vimarsh Puneet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
